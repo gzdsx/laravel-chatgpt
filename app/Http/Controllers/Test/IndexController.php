9cede82ee61b76bd206bb7efe4d37f2509d11126
xy113 <songdewei@163.com>
2023-03-12 23:01:48 +0800

    111

--- app/Http/Controllers/Test/IndexController.php
+++ app/Http/Controllers/Test/IndexController.php
@@ -7,11 +7,40 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
+use Orhanerday\OpenAi\OpenAi;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        
+        $open_ai = new OpenAi('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');
+
+        $complete = $open_ai->chat([
+            'model' => 'gpt-3.5-turbo',
+            'messages' => [
+                [
+                    "role" => "system",
+                    "content" => "You are a helpful assistant."
+                ],
+                [
+                    "role" => "user",
+                    "content" => "Who won the world series in 2020?"
+                ],
+                [
+                    "role" => "assistant",
+                    "content" => "The Los Angeles Dodgers won the World Series in 2020."
+                ],
+                [
+                    "role" => "user",
+                    "content" => "Where was it played?"
+                ],
+            ],
+            'temperature' => 1.0,
+            'max_tokens' => 4000,
+            'frequency_penalty' => 0,
+            'presence_penalty' => 0,
+        ]);
+
+        var_dump($complete);
     }
 }
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,7 +4,7 @@
     <div class="image">
         <img src="{{asset('images/common/404.png')}}">
     </div>
-    <h2>咦，页面有错误，请联系管理员！</h2>
+    <h2>{{$exception->getMessage() ?? '咦，页面有错误，请联系管理员！'}}</h2>
     <div class="links">
         <a href="{{url('/')}}">返回首页</a>
         <a href="{{back()->getTargetUrl()}}">返回上一页</a>
